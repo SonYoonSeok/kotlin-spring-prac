6eab70525709765f933e24974677b744312e9428
SonYoonSeok <dbstjr5517@naver.com>
2021-11-30 17:07:25 +0900

    Initial commit

---
+++ src/test/kotlin/sonny/kotlinspringprac/KotlinSpringPracApplicationTests.kt
@@ -0,0 +1,13 @@
+package sonny.kotlinspringprac
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringPracApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
